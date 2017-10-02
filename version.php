75a05da624fa97fe12bee06eb56b9b6a8e76f254
Amr <amr.hourani@id.ethz.ch>
2017-10-02 15:02:37 +0200

    latest version

--- version.php
+++ version.php
@@ -8,25 +8,24 @@
 //
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Version information for the quizaccess_wifiresilience plugin.
  *
- * @package   quizaccess_wifiresilience
+ * @package quizaccess_wifiresilience
  * @copyright 2017 ETH Zurich (amr.hourani@let.ethz.ch)
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017092600;
-$plugin->requires  = 2015041700;
-$plugin->cron      = 0;
+$plugin->version = 2017100300;
+$plugin->requires = 2015041700;
+$plugin->cron = 0;
 $plugin->component = 'quizaccess_wifiresilience';
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '0.7 for Moodle 3.3+';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '0.9 for Moodle 3.3+';
