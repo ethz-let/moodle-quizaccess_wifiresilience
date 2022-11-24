9813524ff365c708eea762699b2a53ae014b1020
Nexterday <nexterday@users.noreply.github.com>
2022-11-24 15:47:23 +0100

    timestamp for EMDL-617

--- version.php
+++ version.php
@@ -25,7 +25,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->cron = 0;
 $plugin->component = 'quizaccess_wifiresilience';
-$plugin->version = 2022112300;
+$plugin->version = 2022112400;
 $plugin->requires = 2021050100; // Moodle >=3.11+.
 
 $plugin->maturity = MATURITY_STABLE;
