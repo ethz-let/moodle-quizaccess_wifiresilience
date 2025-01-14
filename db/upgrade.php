12c1813cd086f312b473d87607cfe31871a6028a
Nexterday <nexterday@users.noreply.github.com>
2025-01-14 14:35:59 +0100

    Add files via upload

--- db/upgrade.php
+++ db/upgrade.php
@@ -58,5 +58,28 @@ function xmldb_quizaccess_wifiresilience_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2023111300, 'quizaccess', 'wifiresilience');
     }
 
+    /*** tobias */
+    if ($oldversion < 2025012000) {
+        // Define field drawing_usage to control display of result table.
+        $table = new xmldb_table('quizaccess_wifiresilience_sess');
+
+        // Add needed columns.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('attemptid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, false, null);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, false, null);
+        $table->add_field('sesskey', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, false, null);
+
+        // Adding keys to table lti_usage.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('userid', XMLDB_KEY_FOREIGN, ['userid'], 'user', ['id']);
+        $table->add_key('quizid', XMLDB_KEY_FOREIGN, ['attemptid'], 'quiz_attempts', ['id']);
+
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        upgrade_plugin_savepoint(true, 2025012000, 'quizaccess', 'wifiresilience');
+    }
+    /*** end tobias */
     return true;
 }
